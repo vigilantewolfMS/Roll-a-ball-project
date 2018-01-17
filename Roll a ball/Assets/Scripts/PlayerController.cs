41136519f0e41185f43831359536934063ae5041
vigilantewolfMS <32073374+vigilantewolfMS@users.noreply.github.com>
2018-01-17 12:56:38 -0500

    started the project and did all things acording to video and added texture

---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+	public Text countText;
+	
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+		
+	}
+	void SetCountText(){
+		countText.text = "Count: "+ count.ToString();
+	}
+}
